75b5a94754d6a7455b18c1b456b6038b5aa39d89
Rados Mijatovic <radosmijatovic@Radoss-MacBook-Pro.local>
2021-02-16 10:48:48 +0100

    proxy opet

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -23,11 +23,11 @@ import Navbar from "./components/navbar";
 import ClientsScreen from "./screens/ClientsScreen";
 import axios from 'axios';
 
-axios.defaults.baseURL = 'https://novomak-export.com/novomak-export:9000';
+axios.defaults.baseURL = 'https://novomak-export.com/novomak-export3:9000';
 // axios.defaults.headers.common['Authorization'] = 'AUTH TOKEN';
-axios.defaults.headers.post['Content-Type'] = 'application/json';
-axios.defaults.headers.post['Content-Type'] ='application/json;charset=utf-8';
-axios.defaults.headers.post['Access-Control-Allow-Origin'] = '*';
+// axios.defaults.headers.post['Content-Type'] = 'application/json';
+// axios.defaults.headers.post['Content-Type'] ='application/json;charset=utf-8';
+// axios.defaults.headers.post['Access-Control-Allow-Origin'] = '*';
 
 axios.interceptors.request.use(request => {
   console.log(request);
