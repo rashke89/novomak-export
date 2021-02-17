2c111f1df54bfd80230ae152309dbc58b86bd599
Rados Mijatovic <radosmijatovic@Radoss-MacBook-Pro.local>
2021-02-17 15:04:40 +0100

    izmene za bazu

--- backend/config.js
+++ backend/config.js
@@ -13,7 +13,14 @@ export default {
   serverPort,
   mongooseOptions,
   PORT: process.env.PORT || 5000,
-  MONGODB_URL: process.env.MONGODB_URL || 'mongodb://localhost/amazona',
+  MONGODB_URL: () => {
+    console.log(process.env);
+    if (process.env.MONGODB_URL) {
+      console.log(process.env.MONGODB_URL);
+    } else {
+      return 'mongodb://localhost/amazona'
+    }
+  },
   JWT_SECRET: process.env.JWT_SECRET || 'somethingsecret',
   PAYPAL_CLIENT_ID: process.env.PAYPAL_CLIENT_ID || 'sb',
   accessKeyId: process.env.accessKeyId || 'accessKeyId',
