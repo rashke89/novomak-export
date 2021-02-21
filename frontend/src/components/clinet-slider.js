e6eac775453346e60c5b1f2ec7e1095280414368
Rados Mijatovic <radosmijatovic@Radoss-MacBook-Pro.local>
2021-02-21 17:32:36 +0100

    slider

--- frontend/src/components/clinet-slider.js
+++ frontend/src/components/clinet-slider.js
@@ -19,20 +19,43 @@ export default function ClientSlider(props) {
         {slider && Object.keys(slider).length ?
             <div className="brand-wrapper">
                 <h1 className="info-text"><strong className="pr-1">Zastupljeni</strong> brendovi</h1>
-                <div id="carouselExampleIndicators2" className="carousel slide my-5" data-ride="carousel" data-interval={3000}>
+                <div id="carouselExampleSlidesOnly" className="carousel slide" data-ride="carousel">
                     <div className="carousel-inner row">
 
                         {Object.keys(slider).map((item, index) => {
                             return <div key={index} className={`carousel-item ${!index ? 'active' : ''}`}>
                                 {slider[item].map((product) => {
-                                    return <div className="col-lg-3 col-md-6 my-3 img-fluid" key={product._id}>
-                                        <img src={`${config.formatImagePath(product.image)}`} alt=""/>
+                                    return <div className="col-lg-3 col-md-6 my-3" key={product._id}>
+                                        <img className="img-fluid" src={`${config.formatImagePath(product.image)}`} alt=""/>
                                     </div>
                                 })}
                             </div>
                         })}
                     </div>
                 </div>
+                {/*<div id="carouselExampleControls" className="carousel slide" data-ride="carousel">*/}
+                {/*    <div className="carousel-inner">*/}
+                {/*        <div className="carousel-item active">*/}
+                {/*            <img src="..." className="d-block w-100" alt="..."/>*/}
+                {/*        </div>*/}
+                {/*        <div className="carousel-item">*/}
+                {/*            <img src="..." className="d-block w-100" alt="..."/>*/}
+                {/*        </div>*/}
+                {/*        <div className="carousel-item">*/}
+                {/*            <img src="..." className="d-block w-100" alt="..."/>*/}
+                {/*        </div>*/}
+                {/*    </div>*/}
+                {/*    <a className="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">*/}
+                {/*        <span className="carousel-control-prev-icon" aria-hidden="true"></span>*/}
+                {/*        <span className="sr-only">Previous</span>*/}
+                {/*    </a>*/}
+                {/*    <a className="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">*/}
+                {/*        <span className="carousel-control-next-icon" aria-hidden="true"></span>*/}
+                {/*        <span className="sr-only">Next</span>*/}
+                {/*    </a>*/}
+                {/*</div>*/}
+
+
             </div> : ''}
     </>
 }
