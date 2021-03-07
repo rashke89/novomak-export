54c41b6e49ce25caf6cab342cd47afcafaa86a40
Rados Mijatovic <radosmijatovic@Radoss-MacBook-Pro.local>
2021-03-07 18:54:46 +0100

    mail

--- backend/routes/orderRoute.js
+++ backend/routes/orderRoute.js
@@ -7,8 +7,8 @@ import mongoose from 'mongoose';
 const router = express.Router();
 const transport = nodemailer.createTransport({
     host: "smtp.gmail.com",
-    port: 587,
-    secure: false, // true for 465, false for other ports
+    port: 465,
+    secure: true, // true for 465, false for other ports
     auth: {
         user: 'novomak.prodaja@gmail.com', // generated ethereal user
         pass: 'novomak.export2020', // generated ethereal password
